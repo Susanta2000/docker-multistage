36bfb04ef96a86a46520e1e23434fe2b86378703
Susanta2000 <96446078+Susanta2000@users.noreply.github.com>
2024-10-08 11:53:36 +0530

    Update main.py

--- my-project/main.py
+++ my-project/main.py
@@ -1,1 +1,16 @@
 
+import requests
+
+
+def call_api(url: str):
+    try:
+        response = requests.get(url)
+        print(response.json())
+    except Exception as err:
+        print()
+
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/users"
+    call_api(url)
